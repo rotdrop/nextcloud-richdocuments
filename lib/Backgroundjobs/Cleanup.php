4f78d044462ae73ae4e53a35c58aeffa3b142427
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-04-07 15:59:43 +0200

    Remove bogus debug code

--- lib/Backgroundjobs/Cleanup.php
+++ lib/Backgroundjobs/Cleanup.php
@@ -29,7 +29,6 @@ class Cleanup extends TimedJob {
 
 	#[\Override]
 	protected function run($argument) {
-		\OC::$server->get(\OCP\ILogger::class)->info(__METHOD__);
 		// Expire template mappings for file creation
 		$query = $this->db->getQueryBuilder();
 		$query->delete('richdocuments_template')
@@ -49,11 +48,9 @@ class Cleanup extends TimedJob {
 		$query->delete('richdocuments_wopi')
 			->where($query->expr()->in('token', $query->createNamedParameter($tokens, IQueryBuilder::PARAM_INT_ARRAY)));
 		$query->executeStatement();
-		\OC::$server->get(\OCP\ILogger::class)->info(__METHOD__ . ': #EXPIRED: ' . count($tokens));
 		foreach ($tokens as $wopiToken) {
 			$authTokens = $this->tokenProvider->getTokenByUser($wopiToken);
 			foreach ($authTokens as $authToken) {
-				\OC::$server->get(\OCP\ILogger::class)->info('DELETING AUTH TOKEN FOR ' . $wopiToken . ' ' . $authToken->getId());
 				$this->tokenProvider->invalidateTokenById($wopiToken, $authToken->getId());
 			}
 		}
